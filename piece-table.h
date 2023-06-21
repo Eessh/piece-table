c1ae244580113c38b2dc839ed7cd6325749d7b2b
Eessh <eesshkumarjagana@gmail.com>
2023-06-21 15:41:46 +0530

    re-ordered functions in piece_table.h for better readability

--- piece-table.h
+++ piece-table.h
@@ -10,32 +10,44 @@ extern "C"
 
   typedef struct piece_table piece_table;
 
-  /// Piece Table API
+  // Piece Table API
   piece_table* piece_table_new();
+
   piece_table* piece_table_from_string(const char* string);
+
   bool piece_table_insert(piece_table* table,
                           const unsigned int position,
                           const char* string);
+
   bool piece_table_remove(piece_table* table,
                           const unsigned int position,
                           const unsigned int length);
-  char* piece_table_to_string(const piece_table* table);
-  int piece_table_get_length(const piece_table* table);
-  char piece_table_get_char_at(const piece_table* table,
-                               const unsigned int position);
-  char* piece_table_get_slice(const piece_table* table,
-                              const unsigned int position,
-                              const unsigned int length);
-  char* piece_table_get_line(const piece_table* table, const unsigned int line);
+
   bool piece_table_replace(piece_table* table,
                            const unsigned int position,
                            const unsigned int length,
                            const char* string);
+
   bool piece_table_undo(piece_table* table);
+
   bool piece_table_redo(piece_table* table);
+
+  char piece_table_get_char_at(const piece_table* table,
+                               const unsigned int position);
+
+  char* piece_table_get_line(const piece_table* table, const unsigned int line);
+
+  char* piece_table_get_slice(const piece_table* table,
+                              const unsigned int position,
+                              const unsigned int length);
+
+  int piece_table_get_length(const piece_table* table);
+
+  char* piece_table_to_string(const piece_table* table);
+
   bool piece_table_free(piece_table* table);
 
-  /// Loggers
+  // Loggers
   bool piece_table_log(piece_table* table);
 
 #ifdef __cplusplus
